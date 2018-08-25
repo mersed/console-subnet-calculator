03433de8a8ad25997f002de784e5bcc9acb0c93d
mersed <kahrimanovic_mersed@hotmail.com>
2018-08-25 04:00:51 +0200

    Expanding main function. Asking for the ip address insertion when programm is runned.

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-main() {
-	printf("Hello world!\n");
+/**
+ * Main function, which will receive argument as ip address, or
+ * if you dont provide argument, you will be ask to insert it during the code
+ * execution.
+ */
+int main(int argc, char **argv) {
+	char ip_address[17];
+
+	printf("Insert IP address (decimal dotted notation): ");
+	fgets(ip_address, 17, stdin);
+
+	printf("Inserted ip address %s\n", ip_address);
+	return 0;
 }
